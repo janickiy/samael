ffc79ed355e9e10c9b822f95d0761af5289ee318
janickiy <janickiy@mail.ru>
2018-01-22 17:34:51 +0300

    Добпавлен дизайн главной страницы

--- resources/views/layouts/frontend/app.blade.php
+++ resources/views/layouts/frontend/app.blade.php
@@ -4,12 +4,10 @@
 
     {!! Html::style('css/style.css') !!}
 
-    {!! Html::style('css/lightslider.css') !!}
-
     {!! Html::style('assets/plugins/select2/select2.min.css') !!}
 
     <!-- iCheck for checkboxes and radio inputs -->
-        {!! Html::style('assets/plugins/iCheck/all.css') !!}
+    {!! Html::style('assets/plugins/iCheck/all.css') !!}
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
@@ -20,30 +18,344 @@
     {!! Html::script('js/jquery-1.11.1.min.js') !!}
     {!! Html::script('js/select.js') !!}
     {!! Html::script('js/jquery.maskedinput.js') !!}
-    {!! Html::script('js/lightslider.js') !!}
     {!! Html::script('js/jquery.fancybox.js') !!}
 
     @yield('css')
 
 </head>
 <body>
-<div class="site_wrapper">
-    <div class="site row">
-        @include('layouts.frontend.includes.header')
-        <div class="page main_width">
 
-        @yield('marks')
 
-        @include('layouts.frontend.includes.breadcrumbs')
+<div class="site_wrapper row">
 
-        @include('layouts.frontend.includes.notifications')
-        @yield('content')
+    @include('layouts.frontend.includes.header')
 
-        <!-- CONTENT-WRAPPER SECTION END-->
+
+    <div class="main_banner"></div>
+    <div class="index_marks">
+        <div class="main_width">
+            <ul class="row">
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/Hyundai.png" /></div>
+                        <span>Hyundai</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/Renault.png" /></div>
+                        <span>Renault</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/Lada.png" /></div>
+                        <span>Lada</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/Kia.png" /></div>
+                        <span>Kia</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/Nissan.png" /></div>
+                        <span>Nissan</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/Toyota.png" /></div>
+                        <span>Toyota</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/Skoda.png" /></div>
+                        <span>Skoda</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/Datsun.png" /></div>
+                        <span>Datsun</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/Mitsubishi.png" /></div>
+                        <span>Mitsubishi</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/Lifan.png" /></div>
+                        <span>Lifan</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/Ssangyong.png" /></div>
+                        <span>Ssangyong</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/UAZ.png" /></div>
+                        <span>UAZ</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/GreatWall.png" /></div>
+                        <span>Great Wall</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/Ravon.png" /></div>
+                        <span>Ravon</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/Geely.png" /></div>
+                        <span>Geely</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/Ford.png" /></div>
+                        <span>Ford</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/Mazda.png" /></div>
+                        <span>Mazda</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/Haval.png" /></div>
+                        <span>Haval</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/Suzuki.png" /></div>
+                        <span>Suzuki</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/Chery.png" /></div>
+                        <span>Chery</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/Changan.png" /></div>
+                        <span>Changan</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div><img src="images/marks/Volksvagen.png" /></div>
+                        <span>Volksvagen</span>
+                    </a>
+                </li>
+
+            </ul>
         </div>
-
-        @include('layouts.frontend.includes.footer')
     </div>
+    <div class="index_page">
+        <div class="main_width">
+            <div class="presents_block row container">
+                <div class="present_item_container">
+                    <div class="present_item">
+                        <div><img src="images/p_1.png" /></div>
+                        <div>Зимняя резина<br/>в подарок</div>
+                    </div>
+                </div>
+                <div class="present_item_container">
+                    <div class="present_item">
+                        <div><img src="images/p_2.png" /></div>
+                        <div>Дорога<br/>до Москвы<br/>за наш счет</div>
+                    </div>
+                </div>
+                <div class="present_item_container">
+                    <div class="present_item">
+                        <div><img src="images/p_3.png" /></div>
+                        <div>КАСКО<br/>в подарок</div>
+                    </div>
+                </div>
+                <div class="present_item_container">
+                    <div class="present_item">
+                        <div><img src="images/p_4.png" /></div>
+                        <div>Распродажа<br/>авто 2016</div>
+                    </div>
+                </div>
+            </div>
+            <div class="specials_block row">
+                <div class="specials_block_title">Специальные предложения</div>
+                <ul class="row item_list container">
+                    <li>
+                        <div class="list_item">
+                            <a href="">
+                                <div class="item_image"><img src="images/item_1.jpg" /></div>
+                                <div class="item_name">Huyndai i30 Седан</div>
+                                <div class="item_price">от <span>657 000</span> руб.</div>
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="list_item">
+                            <a href="">
+                                <div class="item_image"><img src="images/item_2.jpg" /></div>
+                                <div class="item_name">Huyndai i30 Седан</div>
+                                <div class="item_price">от <span>657 000</span> руб.</div>
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="list_item">
+                            <a href="">
+                                <div class="item_image"><img src="images/item_1.jpg" /></div>
+                                <div class="item_name">Huyndai i30 Седан</div>
+                                <div class="item_price">от <span>657 000</span> руб.</div>
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="list_item">
+                            <a href="">
+                                <div class="item_image"><img src="images/item_2.jpg" /></div>
+                                <div class="item_name">Huyndai i30 Седан</div>
+                                <div class="item_price">от <span>657 000</span> руб.</div>
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="list_item">
+                            <a href="">
+                                <div class="item_image"><img src="images/item_1.jpg" /></div>
+                                <div class="item_name">Huyndai i30 Седан</div>
+                                <div class="item_price">от <span>657 000</span> руб.</div>
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="list_item">
+                            <a href="">
+                                <div class="item_image"><img src="images/item_2.jpg" /></div>
+                                <div class="item_name">Huyndai i30 Седан</div>
+                                <div class="item_price">от <span>657 000</span> руб.</div>
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="list_item">
+                            <a href="">
+                                <div class="item_image"><img src="images/item_1.jpg" /></div>
+                                <div class="item_name">Huyndai i30 Седан</div>
+                                <div class="item_price">от <span>657 000</span> руб.</div>
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="list_item">
+                            <a href="">
+                                <div class="item_image"><img src="images/item_2.jpg" /></div>
+                                <div class="item_name">Huyndai i30 Седан</div>
+                                <div class="item_price">от <span>657 000</span> руб.</div>
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="index_page_2">
+        <div class="main_width">
+            <div class="container row">
+                <div class="soc_block">
+                    <h2>Мы в соцсетях</h2>
+                    <ul>
+                        <li>
+                            <div class="article row">
+                                <a href="">
+                                    <div class="cs_mark"></div>
+                                    <div class="article_image" style="background-image:url(images/a_1.jpg);"></div>
+                                    <p>Знаешь, у тебя красивое имя, изящные формы и пылкий характер. Каждый раз тембр твоего мотора все сильнее разжигает пламя в моем сердце и поток чистых эмоций</p>
+                                </a>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="article row">
+                                <a href="">
+                                    <div class="cs_mark"></div>
+                                    <div class="article_image" style="background-image:url(images/a_2.jpg);"></div>
+                                    <p>Встречайте #Nissan #Qashqai Quantum Edition – дебютанта в обновленном модельном ряду легендарного кроссовера! Новинка представлена в версиях QE и QE+… зависимости от комплектации оснащена системой «ЭРА-ГЛОНАСС»</p>
+                                </a>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="article row">
+                                <a href="">
+                                    <div class="cs_mark"></div>
+                                    <div class="article_image" style="background-image:url(images/a_1.jpg);"></div>
+                                    <p>Знаешь, у тебя красивое имя, изящные формы и пылкий характер. Каждый раз тембр твоего мотора все сильнее разжигает пламя в моем сердце и поток чистых эмоций</p>
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                    <a href="" class="btn">Еще новости</a>
+                </div>
+                <div class="review_block">
+                    <h2>Отзывы</h2>
+                    <ul>
+                        <li>
+                            <div class="review_item row">
+                                <div class="user_name">Изольда Грустная</div>
+                                <div class="review_date">12 марта</div>
+                                <div class="user_image" style="background-image:url(images/u_1.jpg);"></div>
+                                <p>Страх, тревожность и агрессия. Депрессия проявляется как в меланхолии, так и в несвойственном ранее человеку беспокойстве, бессоннице… тревожности. Именно такой вид депрессии чаще приводит к самоубийству, ведь нервная система постоянно напряжена, и человек «устаёт жить».</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="review_item row">
+                                <div class="user_name">Артем Ножик</div>
+                                <div class="review_date">12 марта</div>
+                                <div class="user_image" style="background-image:url(images/u_2.jpg);"></div>
+                                <p>Но распознать признаки суицидального поведения у человека из своего близкого окружения довольно сложно, поэтому мы собрали…</p>
+                            </div>
+                        </li>
+                    </ul>
+                    <a href="" class="btn">Еще отзывы</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="action_block">
+        <div class="main_width">
+            <div class="action_conditions">
+                <div class="action_title_black">Успей поймать ее!</div>
+                <div class="action_title_white">скидка 90 000 руб. </div>
+                <div class="action_title_timer">До конца акции осталось</div>
+                <div class="eTimer custom_timer"></div>
+                <a href="" class="btn">Получить скидку</a>
+            </div>
+        </div>
+    </div>
+    <div class="bottom_map">
+
+    </div>
+
+    @include('layouts.frontend.includes.footer')
+
 </div>
 
 <!-- Bootstrap 3.3.5 -->
--- resources/views/layouts/frontend/includes/header.blade.php
+++ resources/views/layouts/frontend/includes/header.blade.php
@@ -1,25 +1,31 @@
-<header>
-    <div class="top row">
-        <div class="main_width">
-            <div class="address">{!! getSetting('FRONTEND_ADDRESS') !!}</div>
+<header class="row">
+    <div class="main_width">
+        <a href="/" class="logo"><img src="/images/logo.png" /></a>
+        <div class="header_info">
+            <div class="addresses">{!! getSetting('FRONTEND_ADDRESS') !!}</div>
             <div class="times">{!! getSetting('FRONTEND_TIMES') !!}</div>
-            <a href="tel:+78123132274">{!! getSetting('TELEPHONE_1') !!}</a>
-            <a href="tel:+78005001463">{!! getSetting('TELEPHONE_2') !!}</a>
         </div>
-    </div>
-    <div class="header row">
-        <div class="main_width">
-            <a href="/" class="logo"><img src="/{!! getSetting('SITE_LOGO') !!}" /></a>
-            <nav>
-                <ul>
-                    @foreach(getMenuItems('HEADER') as $item)
-                        <li><a href="{{ url($item->url) }}"><b>{{ $item->title }}</b></a></li>
-                    @endforeach
-                </ul>
-            </nav>
+        <div class="header_phones row">
+            <a class="default_phone" href="tel:{!! getSetting('TELEPHONE_1') !!}">{!! getSetting('TELEPHONE_1') !!}</a>
+            <div class="free_phone">
+                <a href="tel:{!! getSetting('TELEPHONE_2') !!}">{!! getSetting('TELEPHONE_2') !!}</a>
+                <span>(Звонок по России бесплатный)</span>
+            </div>
         </div>
     </div>
 </header>
+<div class="nav_wrapper">
+    <div class="main_width">
+        <nav>
+            <ul class="row">
+                @foreach(getMenuItems('HEADER') as $item)
+                    <li><a href="{{ url($item->url) }}"><b>{{ $item->title }}</b></a></li>
+                @endforeach
+            </ul>
+        </nav>
+    </div>
+</div>
+
 
 
 
